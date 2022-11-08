b24eee5cf2400717e5c022414ee69518c21f6609
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-11-08 08:51:39 +0600

    banner done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="tailorbanner1.jpeg" alt="" className="w-full max-h-90" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
