637d55a7a90ccf22d29953463ccb7f0f110aa59d
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-11-09 23:37:52 +0600

    localStorage token set

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -39,6 +39,7 @@ const Login = () => {
                     .then(res => res.json())
                     .then(data => {
                         console.log(data);
+                        localStorage.setItem('token', data.token);
                     })
 
                 // navigate(from, { replace: true })
